35f8b7b13fd180bc4e9f8923c4b80532e6e85304
Shivaji Narlapuram <Shivaji.Narlapuram@mjunction.in>
2026-09-08 16:30:04 +0530

    latest

--- src/main/java/com/cloud/controller/HomeController.java
+++ src/main/java/com/cloud/controller/HomeController.java
@@ -3,9 +3,9 @@ package com.cloud.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/java")
 public class HomeController {
-	@GetMapping("/java")
+	@GetMapping("/")
 	public String getLoin() {
 		return "My spring boot app inside the private server";	}
 
